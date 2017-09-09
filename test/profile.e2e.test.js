5fa66cbdbe2704d052664ee41880d9556be74e0b
Nicola Marcacci Rossi <nicolamr@gmail.com>
2017-09-09 05:11:55 +0200

    profile test

---
+++ test/profile.e2e.test.js
@@ -0,0 +1,40 @@
+import cheerio from 'cheerio'
+import pretty from 'pretty'
+import {ObjectID} from 'mongodb'
+import {setup, teardown} from './setup'
+import {generateAndLogUser, setUserData} from './fixtures'
+
+let db
+let browser
+
+beforeAll(async () => {
+    const res = await setup()
+    db = res.db
+    browser = res.browser
+})
+
+afterEach(async () => {
+    await db.dropDatabase()
+})
+
+afterAll(async () => {
+    await teardown({db, browser})
+})
+
+describe('profile', () => {
+    it('displays', async () => {
+        const browserPage = await browser.createPage()
+
+        await generateAndLogUser(browserPage)
+        await setUserData(browserPage)
+        
+        const status = await browserPage.open(`http://localhost:3000/profile`)
+        expect(status).toBe('success')
+
+        const text = await browserPage.property('content')
+        const $ = cheerio.load(text)
+        const page = $('#__next')
+        const html = pretty(page.html())
+        expect(html).toMatchSnapshot()
+    })
+})
